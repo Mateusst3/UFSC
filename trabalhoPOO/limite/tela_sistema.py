8d27feb407209b769ea5eaa6f65a7f361b8b9477
Mateus <mtex613@gmail.com>
2022-01-05 23:53:25 -0300

    adicionando projeto

---
+++ trabalhoPOO/limite/tela_sistema.py
@@ -0,0 +1,8 @@
+class TelaSistema:
+    def tela_sistema(self):
+        print("----------Sistema de Delivery----------")
+        print("O que você deseja fazer?")
+        print("[1] Entrar no sistema")
+        print("[2] Fechar o sistema")
+        opcao = int(input())
+        return opcao
