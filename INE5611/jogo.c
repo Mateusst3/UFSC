5854f7b27ec013200473d536079a62a35e2657c6
Mateus Silva Teixeira <mtex613@gmail.com>
2023-05-18 00:12:45 -0300

    Adicionando trabalho de SO finalizado

--- INE5611/jogo.c
+++ INE5611/jogo.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ncurses.h>
+#include <curses.h>
 #include <time.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 int x;
-struct timespec ts = {0, 40000000L};
+int global_x_nave, global_y_nave, global_x_bomba, global_y_bomba;
+int global_x_canhao = 40;
+int contador = 0;
+struct timespec ts = {0, 50000000L};
+struct timespec tsNave;
+struct timespec tsNaveEasy = {0, 30000000L};
+struct timespec tsNaveMedium = {0, 20000000L};
+struct timespec tsNaveHard = {0, 10000000L};
+pthread_t idThreadNave, idThreadBomba, idCanhao, idContador;
+int contador_tempo = 0;
+bool respondido = false;
 
-// Fun��o gotoxy
+// Função gotoxy
 void gotoxy(int x, int y)
 {
 	printf("\033[%d;%df", y, x);
@@ -22,6 +32,7 @@ void bomba_horizontal(int x, int y)
 	gotoxy(x + 1, y);
 	printf("___");
 }
+
 void apaga_bomba_horizontal(int x, int y)
 {
 	gotoxy(x, y);
@@ -29,6 +40,7 @@ void apaga_bomba_horizontal(int x, int y)
 	gotoxy(x + 1, y);
 	printf("   ");
 }
+
 void bomba(int x, int y)
 {
 	gotoxy(x, y);
@@ -38,6 +50,7 @@ void bomba(int x, int y)
 	gotoxy(x - 1, y + 2);
 	printf("/||\\");
 }
+
 void apaga_bomba(int x, int y)
 {
 	gotoxy(x, y);
@@ -47,6 +60,7 @@ void apaga_bomba(int x, int y)
 	gotoxy(x - 1, y + 2);
 	printf("    ");
 }
+
 void explode_bomba(int x, int y)
 {
 	gotoxy(x, y);
@@ -81,6 +95,7 @@ void explode_bomba(int x, int y)
 	gotoxy(x, y + 2);
 	printf(" ");
 }
+
 void nave(int x, int y)
 {
 	gotoxy(x, y);
@@ -94,6 +109,7 @@ void nave(int x, int y)
 	gotoxy(x, y + 1);
 	printf("V-v-V");
 }
+
 void apaga_nave(int x, int y)
 {
 	gotoxy(x, y);
@@ -107,6 +123,7 @@ void apaga_nave(int x, int y)
 	gotoxy(x, y + 1);
 	printf("     ");
 }
+
 void canhao(int x, int y)
 {
 	gotoxy(x, y);
@@ -118,7 +135,36 @@ void canhao(int x, int y)
 	gotoxy(x, y + 3);
 	printf("+-+");
 }
-void *ThreadFuncNave(void *linha);
+
+void apaga_canhao(int x, int y)
+{
+	{
+		gotoxy(x, y);
+		printf("  ");
+		gotoxy(x, y + 1);
+		printf("   ");
+		gotoxy(x, y + 2);
+		printf("   ");
+		gotoxy(x, y + 3);
+		printf("   ");
+	}
+}
+
+void atualiza_contador()
+{
+	gotoxy(2, 40);
+	char str[5];
+	sprintf(str, "%d", contador);
+	printf("Abates: %s\n", str);
+}
+
+void *ThreadAtualizaContador()
+{
+	while (5)
+	{
+		atualiza_contador();
+	}
+}
 
 void *ThreadFuncNave(void *linha)
 {
@@ -126,106 +172,128 @@ void *ThreadFuncNave(void *linha)
 	n = (int)linha;
 	for (int x = 79; x >= 0; x--)
 	{
+		global_x_nave = x;
+		global_y_nave = linha;
 		nave(x, linha);
-		nanosleep(&ts, NULL);
+		nanosleep(&tsNave, NULL);
 		apaga_nave(x, linha);
 	}
+	return NULL;
 }
 
-void *ThreadFuncBomba(void *arg);
 void *ThreadFuncBomba(void *arg)
 {
-	for (int y = 20; y >= 5; y--)
+	while (5)
 	{
-		bomba(40, y);
+		int x_canhao = global_x_canhao;
+		int x_bomba = global_x_canhao;
+		int ch = getch();
+		if (ch == ' ')
+		{
+			for (int y = 20; y >= 0; y--)
+			{
+				if ((global_x_bomba == global_x_nave && global_x_bomba == global_x_nave))
+				{
+					explode_bomba(global_x_bomba, global_y_bomba);
+					pthread_cancel(idThreadNave);
+					contador++;
+				}
+				else
+				{
+					bomba(x_bomba, y);
+					global_y_bomba = y;
+					global_x_bomba = 40;
+					nanosleep(&ts, NULL);
+					apaga_bomba(x_bomba, y);
+				}
+			}
+		}
+		if (ch == 'a')
+		{
+			x_canhao = x_canhao - 2;
+		}
+		else if (ch == 'd')
+		{
+			x_canhao = x_canhao + 2;
+		}
+		global_x_canhao = x_canhao;
+		canhao(x_canhao, 20); // Atualiza a posição do canhão na tela
 		nanosleep(&ts, NULL);
-		apaga_bomba(40, y);
+		apaga_canhao(x_canhao, 20);
 	}
+
 	return NULL;
 }
 
 int main()
 {
-	char montanhas[] = {10, 17, 14, 18, 16, 19, 12, 18, 13, 24, 10, 17, 14, 18, 16, 19, 12, 18, 13, 24, 10, 17, 14, 18, 16, 19, 12, 18, 13, 24, 10, 17, 14, 18, 16, 19, 12, 18, 13, 24};
+	printf("Qual a dificuldade? facil, medio ou dificil: ");
 	int coluna = 5;
 	int linha = 3;
 	int k = 0;
-	system("cls");
-	for (int i = 0; i < 18;)
-	{
-		// printf("aqui");
-		for (int j = montanhas[i]; j <= montanhas[i + 1]; j++)
-		{
-			gotoxy(k, j); // printf("%i %i\n", k, j);//
-			printf("\\");
-			k++;
-		}
-
-		for (int j = montanhas[i]; j >= montanhas[i + 1]; j--)
-		{
-			gotoxy(k, j); // printf("%i %i\n", k, j);//
-			printf("/");
-			k++;
-		}
-		i++;
-	}
 	// Providing a seed value
 	srand(time(NULL));
-	k = 21;
+	// k = 21;
 
-	canhao(40, 23);
-	bomba_horizontal(43, 26);
-	bomba_horizontal(43, 25);
-	bomba_horizontal(43, 24);
-	bomba_horizontal(43, 23);
-	bomba_horizontal(43, 22);
-	bomba_horizontal(43, 21);
-	// while (5)
-	// {
-	// 	linha = 2 + (rand() % 5);
-	// 	pthread_t idThread;
-	// 	pthread_create(&idThread, NULL, ThreadFuncNave, linha);
-	// 	pthread_join(idThread, NULL);
-	// 	// for (int x = 79; x >= 0; x--)
-	// 	// {
-	// 	// 	nave(x, linha);
-	// 	// 	nanosleep(&ts, NULL);
-	// 	// 	apaga_nave(x, linha);
-	// 	// }
-	// 	apaga_bomba_horizontal(43, k);
-	// 	for (int y = 20; y >= 5; y--)
-	// 	{
-	// 		bomba(40, y);
-	// 		nanosleep(&ts, NULL);
-	// 		apaga_bomba(40, y);
-	// 	}
-	// 	explode_bomba(40, 7);
-	// 	k++;
-	// 	if (k == 26)
-	// 	{
-	// 		bomba_horizontal(43, 26);
-	// 	}
-	// }
-	while (5)
+	initscr();			   // Inicializa o modo curses
+	cbreak();			   // Desabilita o buffer de linha
+	noecho();			   // Desabilita o eco do caractere digitado
+	nodelay(stdscr, TRUE); // Torna getch() uma função não bloqueante
+	while (!respondido)
 	{
+		char str[50];
+		char str2[50] = "facil";
+		char str3[50] = "medio";
+		char str4[50] = "dificil";
+		int i, i2, i3;
+		scanf("%s", &str);
+		i = strcmp(str, str2);
+		i2 = strcmp(str, str3);
+		i3 = strcmp(str, str4);
+		if (i == 0)
+		{
+			tsNave = tsNaveEasy;
+			respondido = true;
+		}
+		else if (i2 == 0)
+		{
+			tsNave = tsNaveMedium;
+			respondido = true;
+		}
+		else if (i3 == 0)
+		{
+			tsNave = tsNaveHard;
+			respondido = true;
+		}
+	}
+
+	pthread_create(&idThreadBomba, NULL, ThreadFuncBomba, NULL);
+	atualiza_contador();
+	while (contador < 20 && contador_tempo <= 80)
+	{
+		atualiza_contador();
 		linha = 2 + (rand() % 5);
 		int linha2 = 1 + (rand() % 5);
 		int linha3 = 3 + (rand() % 5);
-		pthread_t idThreadNave, idThreadBomba;
 		pthread_create(&idThreadNave, NULL, ThreadFuncNave, (void *)linha);
-		pthread_create(&idThreadNave, NULL, ThreadFuncNave, (void *)linha2);
-		pthread_create(&idThreadNave, NULL, ThreadFuncNave, (void *)linha3);
-
-		pthread_create(&idThreadBomba, NULL, ThreadFuncBomba, NULL);
 		pthread_join(idThreadNave, NULL);
-		pthread_join(idThreadBomba, NULL);
-		apaga_bomba_horizontal(43, k);
-		explode_bomba(40, 7);
-		k++;
-		if (k == 26)
-		{
-			bomba_horizontal(43, 26);
-		}
+		contador_tempo++;
+		system("clear");
 	}
-	getch();
+
+	if (contador_tempo >= 80 && contador <= 20)
+	{
+		pthread_cancel(idThreadNave);
+		pthread_cancel(idThreadBomba);
+		printf("Voce perdeu!");
+	}
+
+	if (contador_tempo <= 80 && contador >= 20)
+	{
+		pthread_cancel(idThreadNave);
+		pthread_cancel(idThreadBomba);
+		printf("Voce ganhou!");
+	}
+
+	return 0;
 }
